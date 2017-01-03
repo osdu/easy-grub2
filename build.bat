07456f3561cce7b8985117a67f4a0772d4c8a489
a1ive <ligenlive@gmail.com>
2017-01-03 21:14:23 +0000

    Support i386-EFI

--- build.bat
+++ build.bat
@@ -5,5 +5,7 @@ echo %modules%
 
 echo build x86_64-efi
 grub-mkimage\grub-mkimage.exe -m memdisk.cpio -d x86_64-efi -p (memdisk)/boot/grub -c config.cfg -o grubx64.efi -O x86_64-efi %modules%
+echo build i386-efi
+grub-mkimage\grub-mkimage.exe -m memdisk.cpio -d i386-efi -p (memdisk)/boot/grub -c config.cfg -o grubia32.efi -O i386-efi %modules%
 
 del memdisk.cpio
--- build.sh
+++ build.sh
@@ -10,5 +10,8 @@ modules=$(cat mods.lst)
 echo $modules
 
 echo "using $mkimage"
+echo "building x86_64-efi"
 ./grub-mkimage/$mkimage -m memdisk.cpio -d ./x86_64-efi -p "(memdisk)/boot/grub" -c config.cfg -o grubx64.efi -O x86_64-efi $modules
+echo "building i386-efi"
+./grub-mkimage/$mkimage -m memdisk.cpio -d ./i386-efi -p "(memdisk)/boot/grub" -c config.cfg -o grubia32.efi -O i386-efi $modules
 rm memdisk.cpio
